8ec0e2fbb62c51165a6b88307d42182cdbf4ea0f
IgbeAjaga <ajagaigbe144@gmail.com>
2024-01-16 23:45:21 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python3
-""" Main application module
+"""
+Main module of the API
 """
 
+
 from flask import Flask, jsonify, abort, request
-from api.v1.views.index import app_views
+from api.v1.views import app_views
+from models import storage
+from os import getenv
 from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-auth_type = app.config.get('AUTH_TYPE')
-auth = BasicAuth() if auth_type == 'basic_auth' else Auth()
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
 
 @app.before_request
 def before_request():
-    """ Before request handler
     """
-    if auth is None or not auth.require_auth(request.path, [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    Before request handler.
+    """
+    if auth is None:
+        return
+    excluded_paths = [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", default="0.0.0.0")
+    port = getenv("API_PORT", default=5000)
+    app.run(host=host, port=port, threaded=True)
