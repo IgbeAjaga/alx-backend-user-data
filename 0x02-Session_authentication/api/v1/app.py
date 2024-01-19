888a3e42264ee4ce6828059e8f0f9f2d4b1439a4
Igbe Ajaga <ajagaigbe144@gmail.com>
2024-01-19 01:27:14 +0100

    authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,53 +1,27 @@
 #!/usr/bin/env python3
 """
-Main application file for Holberton School's backend project.
+Route module for the API
 """
-
-
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.app import app
-from api.v1.auth import Auth, BasicAuth, SessionAuth
-from api.v1.views.index import app_views
-from flask import request, abort
 
 
-class AuthSelector:
-    """
-    AuthSelector class to manage authentication types.
-    """
-    def __init__(self):
-        """
-        Initializes AuthSelector with basic and session
-        authentication instances.
-        """
-        self.auth_basic = BasicAuth()
-        self.auth_session = SessionAuth()
-
-    def select_auth(self) -> Auth:
-        """
-        Selects the appropriate authentication type
-        based on the environment variable AUTH_TYPE.
-        Returns:
-            Auth: Selected authentication instance.
-        """
-        if os.getenv('AUTH_TYPE') == 'session_auth':
-            return self.auth_session
-        return self.auth_basic
-
-
-auth_selector = AuthSelector()
-auth = auth_selector.select_auth()
-app.before_request(auth.require_auth)
-
+app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.before_request
-def before_request():
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
     """
-    Before request method for additional authentication checks.
-    """
-    excluded_paths = ['/api/v1/auth_session/login/']
-    if request.path not in excluded_paths and not auth.authorization_header(
-            request) and not auth.session_cookie(request):
-        abort(401)
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
